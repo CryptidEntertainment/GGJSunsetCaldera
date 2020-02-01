75b99d5f8a81685e544812b9e3235c0f37b29bd5
ScottT4ylor <G00236985@smail.raritanval.edu>
2020-01-31 21:59:37 -0500

    Added new files

---
+++ Assets/Scripts/Objects/InteractiveObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    private bool interacting;
+    public float weight;
+
+
+}
---
+++ Assets/Scripts/Player/GravityGun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGun : MonoBehaviour
+{
+    private bool pickupActive;
+    private InteractiveObject currentTarget;
+
+    void Update
+    {
+        if(Input.GetAxis("Fire1")>0)
+        {
+
+        }
+    }
+
+}
