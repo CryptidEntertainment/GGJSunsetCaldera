bd75f73307f957d82fefb5abbdbbedc7b6992ab2
dragonite <mpeng@optimum.net>
2020-02-01 01:18:18 -0500

    Enemies can chuck stuff at you

---
+++ Assets/Scripts/Etc/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Peng {
+    public class Projectile : MonoBehaviour {
+        public float speed = 1f;
+        public Vector3 direction = Vector3.zero;
+        
+        void Update() {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}
