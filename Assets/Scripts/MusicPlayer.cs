e3cb70a1ec0e1ee8468c086f12ce4474f9147ab3
ScottT4ylor <G00236985@smail.raritanval.edu>
2020-02-02 13:31:47 -0500

    Added music files

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    int audSwitcher=1;
+    AudioSource aud1;
+    AudioSource aud2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
