2ebea297715f11d4a06588cac3ee7b2c36419b2f
dragonite <mpeng@optimum.net>
2020-02-02 12:15:54 -0500

    UI for health bar

---
+++ Assets/HealthMeter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Peng {
+    public class HealthMeter : MonoBehaviour {
+        public List<Sprite> sprites;
+
+        void Update() {
+            int amount = ((int)Mathf.Floor(Player.Me.Health));
+            Image image = GetComponent<Image>();
+            image.sprite = sprites[amount];
+        }
+    }
+}
